dae0c197a0296b22bd983469215ef5788c68b551
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-03 00:59:58 +0200

    Showing the scores data in order to finish the project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import Score from './modules/score.js';
 
 const nameInput = document.querySelector('.name');
 const scoreInput = document.querySelector('.score');
-
-const score = new Score();
+const listScores = document.querySelector('.score-data');
+const refresh = document.querySelector('.refresh');
+const score = new Score(nameInput.value, scoreInput.value);
 
 const form = document.querySelector('.form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  score(nameInput.value, scoreInput.value);
+  score.postScore(nameInput.value, scoreInput.value);
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
+refresh.addEventListener('click', () => {
+  window.location.reload();
+});
+
+window.addEventListener('load', () => {
+  score.getScore(listScores);
 });
